5454ca0a2e924d624da0a9aa49d67ca22ba5b2f2
Ricky Chuang <songsongyyohya@gmail.com>
2021-11-03 14:08:28 +0800

    Initial Commit

---
+++ My Favorite NETFLIX/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  My Favorite NETFLIX
+//
+//  Created by 莊智凱 on 2021/11/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ My Favorite NETFLIX/My_Favorite_NETFLIXApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_Favorite_NETFLIXApp.swift
+//  My Favorite NETFLIX
+//
+//  Created by 莊智凱 on 2021/11/3.
+//
+
+import SwiftUI
+
+@main
+struct My_Favorite_NETFLIXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
